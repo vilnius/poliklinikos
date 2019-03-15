223f62c2fd9707d96f5bde8ca80d669339c639a6
antakpol <andrius.semenauskas@antakpol.lt>
2019-03-15 15:10:24 +0200

    update

--- data/Antakalnio_poliklinika/Registracija/Savivaldybes_importas.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-cd C:\MEDIS\Antakalnio_registracijos_savivaldybei\poliklinikos\data\Antakalnio_poliklinika\Registracija
-Powershell.exe -executionpolicy remotesigned -File  C:\MEDIS\Antakalnio_registracijos_savivaldybei\poliklinikos\data\Antakalnio_poliklinika\Registracija\testas.ps1
